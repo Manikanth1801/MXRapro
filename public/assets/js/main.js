b2bd0a65f2f059ec7b27735f82162d610a872da9
Ankit Sharma <ankit.bhadanitech@gmail.com>
2020-12-04 19:21:20 +0530

    counter add

---
+++ public/assets/js/main.js
@@ -0,0 +1,74 @@
+$(function () {
+  var a = 0;
+  $(window).scroll(function () {
+    // alert();
+    var oTop = $("#counter").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $(".counter-value").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+
+          {
+            duration: 4000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+              //alert('finished');
+            },
+          }
+        );
+      });
+      a = 1;
+    }
+  });
+
+  // alert();
+  $(".heroRightSide").effect("slide", 2000);
+
+  new WOW().init();
+
+  wow = new WOW({
+    boxClass: "who-we-are", // default
+    animateClass: "", // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true, // default
+  });
+
+  $("input[name='customRadio']").click(function () {
+    if ($("#customRadioInline2").is(":checked")) {
+      $("#test").addClass("d-block");
+    }
+    if ($("#customRadioInline1").is(":checked")) {
+      $("#test").removeClass("d-block");
+    } else {
+      $("#test").hide();
+    }
+  });
+});
+
+function toggleClass() {
+  // alert();
+  if ("input:radio:checked") {
+    $(".card").toggleClass("is-flipped");
+  }
+
+  // $("input:radio:checked").next('label').addClass("is-flipped");
+  console.log("????", this);
+}
+
+var rellax = new Rellax(".rellax");
+
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
